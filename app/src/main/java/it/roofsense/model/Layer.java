e7364af5adf376dbcdb09787c7ed61eecadfc727
Riccardo Quercetti <riccardo.quercetti.93@gmail.com>
2024-01-24 19:24:21 +0100

    chore: project structure conformed to lab classes specifications

--- app/src/main/java/it/roofsense/model/Layer.java
+++
@@ -1,5 +0,0 @@
-package it.roofsense.model;
-
-public class Layer {
-    
-}
---
+++ src/main/java/it/roofsense/Backend.java
@@ -0,0 +1,15 @@
+package it.roofsense;
+
+/**
+ * Application entry point.
+ */
+public final class Backend {
+    private Backend() { }
+    /**
+     * Applcation main method.
+     * 
+     * @param args command line arguments.
+     */
+    public static void main(final String[] args) {
+    }
+}
